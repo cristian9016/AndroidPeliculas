8b8a4cfea2c6fd7a6d1c5c16e73c719220bed59c
Cristian Muñoz <cristian9016@gmail.com>
2018-02-02 09:14:11 -0500

    uploaded to github

--- app/src/main/java/unicauca/movil/peliculas/fragments/MainFragment.kt
+++ app/src/main/java/unicauca/movil/peliculas/fragments/MainFragment.kt
@@ -7,11 +7,9 @@ import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import kotlinx.android.synthetic.main.fragment_main.*
 import org.jetbrains.anko.support.v4.startActivity
 import unicauca.movil.peliculas.DetailActivity
-
 import unicauca.movil.peliculas.R
 import unicauca.movil.peliculas.adapters.PeliculaAdapter
 import unicauca.movil.peliculas.util.Data
